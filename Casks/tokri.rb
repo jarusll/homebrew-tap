27d4c01a565ed768951f98810b2136eabe638c3a
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2026-03-29 22:33:15 +0530

    Update sha of dmg

--- Casks/tokri.rb
+++ Casks/tokri.rb
@@ -1,6 +1,6 @@
 cask "tokri" do
     version "2026.03.01"
-    sha256 "44b370665e061ebe9adfcafb59f7a89823e6e9659d2033dd7be9a8d4c47dde56"
+    sha256 "7e27ade907e12291c65993e38c8cf9b74c842eb1f403abebe2c58760bb46d7e1"
 
     url "https://github.com/jarusll/tokri/releases/download/v#{version}/Tokri.dmg"
     name "Tokri"
